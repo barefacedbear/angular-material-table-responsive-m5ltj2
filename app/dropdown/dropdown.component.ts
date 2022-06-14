4f10eb933c9001f9233a9b695f8ec1bb002038a0
barefacedbear <avishek.arunavo1997@gmail.com>
2022-06-14 17:11:05 +0530

    2 files modified, 1 added

--- app/dropdown/dropdown.component.ts
+++ app/dropdown/dropdown.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dropdown',
@@ -9,7 +10,17 @@ export class DropdownComponent implements OnInit {
 
   constructor() { }
 
+  private subject: Subject<string> = new Subject();
+  z=[1,2,3];
+
   ngOnInit() {
+    this.subject.debounceTime(2000).subscribe(searchTextValue => {
+      this.z.push(...[4,5,6]);
+    });
+  }
+
+  search(event) {
+    this.subject.next(event.target.value);
   }
 
 }
